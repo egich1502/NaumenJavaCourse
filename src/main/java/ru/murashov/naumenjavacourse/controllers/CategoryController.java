f140880e139b8fb9cad6023f49ca602207c20229
egich1502 <mes89myr@mail.ru>
2022-10-24 13:55:29 +0500

    изменил расположение пакетов

--- src/main/java/ru/murashov/naumenjavacourse/controllers/CategoryController.java
+++ src/main/java/ru/murashov/naumenjavacourse/controllers/CategoryController.java
@@ -1,4 +1,4 @@
-package ru.murashov.naumenjavacourse.config.controllers;
+package ru.murashov.naumenjavacourse.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
